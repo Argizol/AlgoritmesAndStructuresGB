7fc80d8d298fb602cc852604de48a0d2c81a6b21
Dmitriy <argizol@mail.ru>
2023-02-14 22:01:29 +0300

    Пирамидальная сортировка

---
+++ Lesson2.cs
@@ -0,0 +1,52 @@
+﻿namespace Algoritmes
+{
+    internal class Lesson2
+    {
+        static int add2pyramid(int[] arr, int i, int N)
+        {
+            int imax;
+            int buf;
+            if ((2 * i + 2) < N)
+            {
+                if (arr[2 * i + 1] < arr[2 * i + 2]) imax = 2 * i + 2;
+                else imax = 2 * i + 1;
+            }
+            else imax = 2 * i + 1;
+            if (imax >= N) return i;
+            if (arr[i] < arr[imax])
+            {
+                buf = arr[i];
+                arr[i] = arr[imax];
+                arr[imax] = buf;
+                if (imax < N / 2) i = imax;
+            }
+            return i;
+        }
+
+        public static void PyramidSort(int[] arr, int len)
+        {
+            //step 1: building the pyramid
+            for (int i = len / 2 - 1; i >= 0; --i)
+            {
+                long prev_i = i;
+                i = add2pyramid(arr, i, len);
+                if (prev_i != i) ++i;
+            }
+
+            //step 2: sorting
+            int buf;
+            for (int k = len - 1; k > 0; --k)
+            {
+                buf = arr[0];
+                arr[0] = arr[k];
+                arr[k] = buf;
+                int i = 0, prev_i = -1;
+                while (i != prev_i)
+                {
+                    prev_i = i;
+                    i = add2pyramid(arr, i, k);
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿int[] arr = new int[100];
+//заполняем массив случайными числами
+Random rd = new Random();
+for (int i = 0; i < arr.Length; ++i)
+{
+    arr[i] = rd.Next(1, 101);
+}
+System.Console.WriteLine("The array before sorting:");
+foreach (int x in arr)
+{
+    System.Console.Write(x + " ");
+}
+//сортировка
+Algoritmes.Lesson2.PyramidSort(arr, arr.Length);
+
+System.Console.WriteLine("\n\nThe array after sorting:");
+foreach (int x in arr)
+{
+    System.Console.Write($"{x} +  ");
+}
+System.Console.WriteLine("\n\nPress the <Enter> key");
+System.Console.ReadLine();
